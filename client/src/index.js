c974bf196eb4ac5daa251a7c92577c17e23cdaa9
Nicolai Gaina <nicolaigaina@gmail.com>
2021-02-03 03:09:43 -0800

    added graphql schema in the server and instanciated apollo client in the front end

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import GlobalStyles from './styles';
-import Pages from './pages';
+import React from "react";
+import ReactDOM from "react-dom";
+import GlobalStyles from "./styles";
+import Pages from "./pages";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:6969/graphql",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <GlobalStyles />
-    <Pages />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <GlobalStyles />
+      <Pages />
+    </React.StrictMode>
+  </ApolloProvider>,
+  document.getElementById("root")
 );
--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,38 @@
-//TODO
+const { ApolloServer, MockList } = require("apollo-server");
+
+const typeDefs = require("./schema");
+const mocks = {
+  Query: () => ({
+    tracksForHome: () => new MockList([6, 9]),
+  }),
+  Track: () => ({
+    id: () => "track_01",
+    title: () => "Astro Kitty, Space Explorer",
+    author: () => ({
+      name: "Grumpy Cat",
+      proto:
+        "https://info.umkc.edu/unews/wp-content/uploads/2016/04/janet_mock.jpg.size_.xxlarge.promo_.jpg",
+    }),
+    thumbnail: () =>
+      "https://media.gettyimages.com/photos/cat-giving-you-a-disdainful-look-picture-id1227403521?s=2048x2048",
+    length: () => 1210,
+    modulesCount: () => 6,
+  }),
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  mocks,
+  playground: {
+    endpoint: "/graphql",
+  },
+});
+
+const PORT = 6969;
+server.listen(PORT).then(() => {
+  console.log(`
+    Server is running!
+    Listening on port ${PORT}
+    Query at https://localhost:6969/
+  `);
+});
